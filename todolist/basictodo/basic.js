8732ecdf21d23a2fbe793336ef3c5e87da497b0b
Siza <laurenadhikari16@gmail.com>
2023-05-30 21:03:59 +0545

    basic todo complete

---
+++ todolist/basictodo/basic.js
@@ -0,0 +1,34 @@
+// variables
+var input = document.getElementById("todo-items");
+var todoList = document.getElementById("todo-list");
+var addButton = document.getElementById("add");
+
+// function to add a task
+function addTask() {
+  var task = input.value.trim();
+
+  if (task !== "") {
+    var listItem = document.createElement("li");
+    var taskText = document.createElement("span");
+    taskText.textContent = task;
+    var deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", deleteTask);
+    listItem.appendChild(taskText);
+    listItem.appendChild(deleteBtn);
+    todoList.appendChild(listItem);
+    input.value = "";
+  }
+}
+
+// function to delete a task
+function deleteTask() {
+  var listItem = this.parentNode;
+  todoList.removeChild(listItem);
+}
+
+// event listener for adding a task
+addButton.addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent form submission
+  addTask();
+});
